e0e897ff4946af1f0f791220bc8e6f9dd315393c
memcat8 <memcat13@gmail.com>
2024-10-17 10:18:21 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,25 +4,25 @@ set -e
 stop update-engine
 rm -rf /usr/bin/update_engine_client
 echo "rm -rf /usr/bin/update_engine_client"
-if [ "$?" != 0 ]; then
+if [ $? != 0 ]; then
  echo "An error has occured! Make sure you have rootfs protection off!"
  exit 1
 fi
 rm -rf /usr/sbin/update_engine
 echo "rm -rf /usr/sbin/update_engine"
-if [ "$?" != 0 ]; then
+if [ $? != 0 ]; then
  echo "An error has occured! Make sure you have rootfs protection off!"
  exit 1
 fi
 rm -rf /usr/sbin/chromeos-firmwareupdate
 echo "rm -rf /usr/sbin/chromeos-firmwareupdate"
-if [ "$?" != 0 ]; then
+if [ $? != 0 ]; then
  echo "An error has occured! Make sure you have rootfs protection off!"
  exit 1
 fi
 rm -rf /opt/google/cr50/firmware/*
 echo "rm -rf /opt/google/cr50/firmware/* this doesn't delete your firmware just cached firmware updates"
-if [ "$?" != 0 ]; then
+if [ $? != 0 ]; then
  echo "An error has occured! Make sure you have rootfs protection off!"
  exit 1
 fi
